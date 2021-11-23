25d29f798fc1d8f26b20b73fc38452b6dfd73019
moshtaghfr <moshtagh.fr@gmail.com>
2021-11-23 15:34:12 +0100

    fetch a rainbow random picture and display it into dom element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const catchRainbow = async () => {
+    try {
+      const response = await fetch(
+        'https://upload.wikimedia.org/wikipedia/commons/f/f7/Double_rainbow.jpg'
+      );
+      const blob = await response.blob();
+      document.getElementById('rainbow').src = URL.createObjectURL(blob);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  catchRainbow();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>hello world</h1>
+      <img src='' id='rainbow' alt='rainbow' height='400' />
     </div>
   );
 }
